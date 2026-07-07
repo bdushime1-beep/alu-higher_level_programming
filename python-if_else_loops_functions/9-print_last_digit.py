8a640249244c1d6c0d02608d7c593f6566e71ba7
bdushime1-beep <b.dushime1@alustudent.com>
2026-07-07 05:04:46 -0700

    Fix print_last_digit to use absolute value

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@
 
 def print_last_digit(number):
     """Print and return the last digit of number."""
-    last_digit = number % 10
+    last_digit = abs(number) % 10
     print("{:d}".format(last_digit), end="")
     return last_digit
