491709704f552274a76d7b681d1ee747f015ef5a
bdushime1-beep <b.dushime1@alustudent.com>
2026-07-07 04:52:59 -0700

    Fix last digit calculation for negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,19 +2,17 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = number % 10
 
-if last_digit < 0:
-    last_digit += 10
+if number < 0:
+    last_digit = -(-number % 10)
+else:
+    last_digit = number % 10
 
-# adjust sign to match number's sign for display
-display_digit = number % 10
-
-if display_digit > 5:
+if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(
-        number, display_digit))
-elif display_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, display_digit))
+        number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
-        number, display_digit))
+        number, last_digit))
